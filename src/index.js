3e2feee3f24f00d87ea10ac6bde9a99e80754fbc
IgrMironov <igrmironov@gmail.com>
2022-02-22 23:11:18 +0300

    fix: task solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,30 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    if (expr.length % 10) {
+        expr = new Array(10 - expr.length % 10).fill(0).join('') + expr;
+    }
+
+    const letters = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        letters.push(expr.slice(i, i + 10))
+    }
+
+    let words = letters.map(item => {
+        let pieces = [];
+        for (let i = 0; i < 10; i += 2) {
+            pieces.push(item.slice(i, i + 2))
+        }
+
+        let morse = pieces.map(item => {
+            if (item === '11') return '-'
+            else if (item === '10') return '.'
+            else if (item === '**') return ' '
+            else return ''
+        })
+        return morse.join('')
+    })
+    return words.map(item => MORSE_TABLE[item] ? MORSE_TABLE[item] : ' ').join('')
 }
 
 module.exports = {
